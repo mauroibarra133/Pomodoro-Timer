e9cf63f0760370ff29decb1d8e96de9834419979
Mauro Ibarra <mauroibarra133@gmail.com>
2023-03-12 04:01:06 -0300

    manage the tab changes, and it`s effects

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 
 
-// Variables
+// mediaQueries
 let mediaQuery400 = window.matchMedia("(min-width: 400px)");
 let mediaQuery600 = window.matchMedia("(min-width: 600px)");
-
+//Tabs
+const pomoTab = document.getElementById('timer-pomo');
+const shortTab = document.getElementById('timer-short');
+const longTab = document.getElementById('timer-long');
 
 function handleMediaQuery(mediaQuery){
     const timerPomo = document.getElementById('timer-pomo');
@@ -39,11 +42,58 @@ function handleMediaQuery(mediaQuery){
     }
 
 }
+
+function changeTab(e){
+    let target = e.target;
+    if(e.target.tagName == 'H2'){
+        target = e.target.parentNode;
+    }
+    for(let tab of [pomoTab,shortTab,longTab]){
+        tab = tab.parentNode
+        if(tab.matches('.timer-pomo-active')){
+            tab.classList.toggle('timer-pomo-active');
+        }
+    }
+    target.classList.toggle('timer-pomo-active');
+    applyTabEffects(target);
+}
+function applyTabEffects(target){
+    console.log(target);
+    const backgroundPage = document.getElementById('body');
+    const timerNumber = document.getElementById('timer-number')
+    const timerAdvice = document.getElementById('timer-advice')
+    const root = document.documentElement.style;
+
+    if(target.childNodes[0].matches('#timer-short')){
+        backgroundPage.style.backgroundColor='#116320';
+        timerNumber.innerHTML = '05:00'
+        timerAdvice.innerHTML = 'Time to take a break!'
+        root.setProperty('----button-bg-hover','#116320')
+
+    }else if(target.childNodes[0].matches('#timer-pomo')){
+        backgroundPage.style.backgroundColor='#ba4949';
+        timerNumber.innerHTML = '25:00'
+        timerAdvice.innerHTML = 'Time to focus!'
+        root.setProperty('----button-bg-hover','#ba4949')
+
+    }else if(target.childNodes[0].matches('#timer-long')){
+        backgroundPage.style.backgroundColor='#31834f';
+        timerNumber.innerHTML = '15:00'
+        timerAdvice.innerHTML = 'Time to take a rest!'
+        root.setProperty('----button-bg-hover','#31834f')
+
+    }
+}
+
+//runs when started
+handleMediaQuery(mediaQuery400)
+handleMediaQuery(mediaQuery600)
 //Event Listeners
 mediaQuery400.addEventListener('change',()=>{handleMediaQuery(mediaQuery400)});
 mediaQuery600.addEventListener('change',()=>{handleMediaQuery(mediaQuery600)});
+pomoTab.addEventListener('click',changeTab);
+shortTab.addEventListener('click',changeTab);
+longTab.addEventListener('click',changeTab);
 
-//
-handleMediaQuery(mediaQuery400)
-handleMediaQuery(mediaQuery600)
+
 
