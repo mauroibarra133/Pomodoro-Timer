89d1fa288d66ae4fb391d4cf51b9f758efcaaab7
Mauro Ibarra <mauroibarra133@gmail.com>
2023-03-12 18:45:21 -0300

    delete the youtube idea

--- youtube.js
+++
@@ -1,4 +0,0 @@
-let youtubeUrl = 'https://youtu.be/tstCJt507aY?t=4'
-let videoId = youtubeUrl.match(/v=([a-zA-Z0-9_-]+)/);
-let apiUrl = 'https://www.googleapis.com/youtube/v3/videos?id=' + videoId + '&part=contentDetails&key=[AIzaSyBFyaJAjZHH12V6BNb6c1BnTHhXnZJaxbM]';
-console.log(apiUrl);
